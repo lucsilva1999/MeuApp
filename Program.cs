937487c2d410e3741a9d4e982a7444c1b441c148
unknown <lucsilva1999@gmail.com>
2020-11-18 13:04:42 -0300

    Primeira aula de C# no curso do André Baltieri (balta.io)

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n1 = 1;
+            int n2 = 3;
+            int total = n1 + n2; 
+            string texto = "Testando";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(texto);
+            Console.WriteLine(total);
+        }
+    }
+}
